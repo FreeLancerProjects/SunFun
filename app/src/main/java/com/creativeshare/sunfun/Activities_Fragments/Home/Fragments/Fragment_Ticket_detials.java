73b68f19cc5e3d4074ca6d7315d0cd60d9c22135
AHMED <ahmedmohamed23345@gmail.com>
2019-07-07 14:44:54 +0200

    CH

--- app/src/main/java/com/creativeshare/sunfun/Activities_Fragments/Home/Fragments/Fragment_Ticket_detials.java
+++ app/src/main/java/com/creativeshare/sunfun/Activities_Fragments/Home/Fragments/Fragment_Ticket_detials.java
@@ -1,11 +1,14 @@
 package com.creativeshare.sunfun.Activities_Fragments.Home.Fragments;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,15 +16,20 @@ import androidx.fragment.app.Fragment;
 
 import com.creativeshare.sunfun.Activities_Fragments.Home.Activity.HomeActivity;
 import com.creativeshare.sunfun.R;
+import com.google.android.material.appbar.AppBarLayout;
 
 import java.util.Locale;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import io.paperdb.Paper;
 
 public class Fragment_Ticket_detials extends Fragment {
     private HomeActivity homeActivity;
     private String cuurent_language;
-
+    private AppBarLayout app_bar;
+    private CircleImageView circleImageView;
+    private TextView tv_detilas;
+    private Button bt_book;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ticket_detials, container, false);
@@ -34,6 +42,25 @@ public class Fragment_Ticket_detials extends Fragment {
         Paper.init(homeActivity);
         cuurent_language = Paper.book().read("lang", Locale.getDefault().getLanguage());
 
+        app_bar = view.findViewById(R.id.app_bar);
+        circleImageView=view.findViewById(R.id.image);
+        bt_book=view.findViewById(R.id.bt_book);
+        app_bar.addOnOffsetChangedListener(new AppBarLayout.BaseOnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+                int total_range = appBarLayout.getTotalScrollRange();
+                if ((total_range+i)<70)
+                {
+                   bt_book.setVisibility(View.GONE);
+                   circleImageView.setVisibility(View.GONE);
+                }else
+                {
+                   bt_book.setVisibility(View.VISIBLE);
+                   circleImageView.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
 
     }
 
